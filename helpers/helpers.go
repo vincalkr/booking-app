c6ec74601bb87a116b568427ebeff5296d982405
Vincenzo Calendini <vincenzocalendini@gmail.com>
2022-01-30 19:21:30 +0100

    initial commit

---
+++ helpers/helpers.go
@@ -0,0 +1,7 @@
+package helpers
+
+import "strings"
+
+func SplitFields(str string) []string {
+	return strings.Fields(str)
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"booking-app/helpers"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(helpers.SplitFields("Hello World"))
+}
